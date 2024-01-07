716811fd48630ab7b38e85d94595aa24b7581508
TREBORDX <robertketting3@gmail.com>
2024-01-07 19:53:31 +1100

    Finalised logic for RPS in console

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ function computerChoice() {
         return "rock"
     } else if (randomNum == 1) {
         return "paper"
-    }else {
+    } else {
         return "scissors"
     };
 };
@@ -18,39 +18,73 @@ function playerChoice() {
     result = choice.toLowerCase();
 
     return result;
-}
+};
 
-const userInput = playerChoice();
-
-const machineInput = computerChoice();
-
-console.log(machineInput)
-
-function playGame (playerInput, computerInput) {
+function playGame(playerInput, computerInput) {
 
     if (playerInput == computerInput) {
         return `Game Tied`
-    
-    }else if (playerInput == "rock" && computerInput == "scissors") {
-        return `Player chose ${playerInput}, Computer chose ${computerInput} \nPlayer Wins!`
-    
-    }else if (playerInput == "paper" && computerInput == "rock"){
-        return `Player chose ${playerInput}, Computer chose ${computerInput} \nPlayer Wins!`
-    
-    }else if (playerInput == "scissors" && computerInput == "paper"){
-        return `Player chose ${playerInput}, Computer chose ${computerInput} \nPlayer Wins!`
-    
-    }else if (playerInput == "scissors" && computerInput == "rock"){
-        return `Player chose ${playerInput}, Computer chose ${computerInput} \nComputer Wins!`
-    
-    }else if (playerInput == "rock" && computerInput == "paper"){
-        return `Player chose ${playerInput}, Computer chose ${computerInput} \nComputer Wins!`
-    
-    }else {
+
+    } else if (playerInput == "rock" && computerInput == "scissors") {
+        return `Player Wins!`
+
+    } else if (playerInput == "paper" && computerInput == "rock") {
+        return `Player Wins!`
+
+    } else if (playerInput == "scissors" && computerInput == "paper") {
+        return `Player Wins!`
+
+    } else if (playerInput == "scissors" && computerInput == "rock") {
+        return `Computer Wins!`
+
+    } else if (playerInput == "rock" && computerInput == "paper") {
+        return `Computer Wins!`
+
+    } else {
         return "Computer Wins"
     }
-}
+};
 
-const game = playGame(userInput, machineInput)
+//best of 5 below
 
-console.log(game)
+let computerScore = 0;
+let playerScore = 0;
+
+while (computerScore < 3 && playerScore < 3) {
+    console.log(playerScore)
+    console.log(computerScore)
+
+    let userInput = playerChoice();
+
+    let machineInput = computerChoice();
+
+    console.log(machineInput)
+
+    game = playGame(userInput, machineInput)
+    console.log(game)
+
+    if (game == "Game Tied") {
+        console.log("Game was Tied, Try again")
+        userInput = playerChoice();
+        machineInput = computerChoice();
+
+        game = playGame(userInput, machineInput);
+        console.log(game)
+
+    } else {
+        console.log(game)
+    }
+
+    if (game == "Computer Wins") {
+        computerScore += 1;
+    } else if (game = "Player Wins") {
+        playerScore += 1;
+    }
+};
+
+if (computerScore == 3) {
+    console.log("Computer won best of 3")
+} else {
+    console.log("Player won best of 3")
+    console.log(`Player score: ${playerScore}. Computer Score: ${computerScore}`)
+};
